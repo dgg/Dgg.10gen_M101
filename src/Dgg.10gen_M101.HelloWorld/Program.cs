a803a58c271ff0e4ea2220bb814896a29e3c255d
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2012-10-23 22:56:49 +0200

    display all elements in the "names" collection of the test database

--- src/Dgg.10gen_M101.HelloWorld/Program.cs
+++ src/Dgg.10gen_M101.HelloWorld/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Dgg._10gen_M101.HelloWorld
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Dgg._10gen_M101.HelloWorld
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			MongoDatabase testDb = MongoDatabase.Create(new MongoUrl("mongodb://localhost:27017/test"));
+
+			MongoCollection<BsonDocument> names = testDb.GetCollection("names");
+			foreach (var name in names.FindAll())
+			{
+				Console.WriteLine(name);
+			}
+
+			Console.WriteLine("...");
+			Console.ReadLine();
 		}
 	}
 }
