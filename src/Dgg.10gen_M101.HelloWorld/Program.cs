c7b891053a5b8323413740ba7e8e78df05a649cc
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2012-10-23 22:42:37 +0200

    very empty Hello World console project

---
+++ src/Dgg.10gen_M101.HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Dgg._10gen_M101.HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ src/Dgg.10gen_M101.HelloWorld/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dgg.10gen_M101.HelloWorld")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dgg.10gen_M101.HelloWorld")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f346f964-4ea1-46c8-a553-e91179cf34e2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
